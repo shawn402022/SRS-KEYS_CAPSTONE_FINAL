26299af427ef122a4e1d36b29af52aa24c48a28f
Shawn <sgiovanni1@yahoo.com>
2023-01-11 01:17:25 -0500

    trying another way to add notes to keys

--- app.js
+++ app.js
@@ -1,60 +1,56 @@
 let numOfOct = 5;
 const octWidth = 560;
 
+const whiteKeyWidth = 80
+const pianoHeight = 400
+
+const naturalNotes= ['C', 'D', 'E', 'F', 'G', 'A', 'B']
+const range = ['F3','A7']
+
 const pianoSVG = 
 `
- <svg
-    width="100%"
-    viewBox= "0 0 ${numOfOct * octWidth } 400"
-    version='1.1'
-    xmlns="http://www.w3.org/2000/svg"
-    xmlns:xlink= "http://www.w3.org/1999/xlink"
-    >
-    <g id ="piano-keyboard">
-    </g>
-</svg>
+    <svg
+        width="100%"
+        viewBox= "0 0 ${numOfOct * octWidth } 400"
+        version='1.1'
+        xmlns="http://www.w3.org/2000/svg"
+        xmlns:xlink= "http://www.w3.org/1999/xlink"
+        >
+        <g id ="piano-keyboard">
+        </g>
+    </svg>
 `;
 
 const piano = document.querySelector("#piano");
 
 const app ={
     setupPiano() {
+        const allNaturalNotes = this.getNaturalNotes(range)
         //  Add main SVG to piano div
         piano.innerHTML = pianoSVG
         const pianoKeyboard = document.querySelector("#piano-keyboard")
 
         //  Creating octaves
         for (let i = 0; i < numOfOct; i++) {
-            // create a group element with a class of octive, just like in html
-            const octave = utils.createSVGElement("g")
-            octave.classList.add("octave")
-            // Moves the new octives that are created
-            octave.setAttribute("transform", `translate(${ i * octWidth }, 0)`)
+            const octave = this.createOctave(i)
             
             let whiteKeyXPosition = 0
             let blackKeyXPosition = 60
             // add white keys to octave
             for (let i = 0; i < 7; i++) {
-                const whiteKey = utils.createSVGElement("rect")
-                whiteKey.classList.add("white-key")
+                const whiteKey = this.createKey({ className: "white-key", width:whiteKeyWidth, height:400})
                 whiteKey.setAttribute("x", whiteKeyXPosition)
-                whiteKey.setAttribute("width", 80)
-                whiteKey.setAttribute("height", 400)
-                whiteKeyXPosition += 80
+                whiteKeyXPosition += whiteKeyWidth
                 octave.appendChild(whiteKey)
             }
             // add black keys to octave
             for (let i = 0; i < 5; i++) {
-                const blackKey = utils.createSVGElement("rect")
-                blackKey.classList.add("black-key")
+                const blackKey = this.createKey({ className: "black-key", width:whiteKeyWidth / 2, height:250})
                 blackKey.setAttribute("x", blackKeyXPosition)
-                blackKey.setAttribute("width", 40)
-                blackKey.setAttribute("height", 250)
-
                 if (i === 1) {
-                    blackKeyXPosition += 160
+                    blackKeyXPosition += whiteKeyWidth * 2 
                 } else{
-                    blackKeyXPosition += 80
+                    blackKeyXPosition += whiteKeyWidth
                 }
                 octave.appendChild(blackKey)
             }
@@ -62,8 +58,61 @@ const app ={
             // add to DOM
             pianoKeyboard.appendChild(octave)
         }
-    }
+    },
 
+    createOctave(octNum) {
+         // create a group element with a class of octive, just like in html
+         const octave = utils.createSVGElement("g")
+         octave.classList.add("octave")
+
+         // Moves the new octives that are created
+         octave.setAttribute("transform", `translate(${ octNum * octWidth }, 0)`)
+         return octave
+    },
+
+    createKey({className, width, height}) {
+        const key = utils.createSVGElement("rect")
+        key.classList.add(className)
+        key.setAttribute("width", width)
+        key.setAttribute("height", height)
+        return key
+    },
+    getNaturalNotes([firstNote, lastNote]) {
+        // Assingn octave number, notes and positoins to variables
+        const firstNoteName = firstNote[0]
+        const firstOctaveNumber = parseInt(firstNote[1])  
+
+        const lastNoteName = lastNote[0]
+        const lastOctaveNumber = parseInt(lastNote[1])
+
+        const firstNotePosition = naturalNotes.indexOf(firstNoteName)
+        const lastNotePosition = naturalNotes.indexOf(lastNoteName)
+
+        const allNaturalNotes = []
+
+        for( let octaveNumber = firstOctaveNumber; octaveNumber <= lastOctaveNumber; octaveNumber++){
+            // first octave
+            if(octaveNumber === firstOctaveNumber) {
+                naturalNotes.slice(firstNotePosition).forEach((noteName) => {
+                    allNaturalNotes.push(noteName + octaveNumber)
+                }) 
+                
+                // last octave
+            } else if (octaveNumber === lastOctaveNumber) {
+                // start at the beginning of array then go to the last position but add 1 so  it ands at "a" instead of 'g'
+                naturalNotes.slice(0, lastNotePosition + 1).forEach((noteName) => {
+                    allNaturalNotes.push(noteName + octaveNumber)
+                })
+
+            } else {
+                naturalNotes.forEach((noteName) => {
+                    allNaturalNotes.push(noteName + octaveNumber)
+                })
+            }
+        }
+        return allNaturalNotes
+    }
+    
 }
 
 const utils = {
@@ -71,7 +120,8 @@ const utils = {
         const element = document.createElementNS("http://www.w3.org/2000/svg", el)
         return element
 
-    }
+    },
        
 }
 app.setupPiano()
+console.log(app.getNaturalNotes(range))
